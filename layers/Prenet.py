15a1e08f3bfeeab67b5f18cebf01af0a5b21262e
Anandaswarup Vadapalli <anandaswarup@bombe.local>
2018-04-28 11:22:26 +0530

    Prenet

--- layers/Prenet.py
+++ layers/Prenet.py
@@ -1,28 +1,34 @@
-"""Prenet"""
+"""Prenet Module"""
 
 import tensorflow as tf
 
 
 class Prenet(tf.keras.Model):
-    """Prenet
+    """Prenet module
     """
     def __init__(self, prenet_layers, dropout):
-        """Prenet
+        """Constructor
         """
         super(Prenet, self).__init__()
 
-        self.prenet1 = tf.keras.layers.Dense(units=prenet_layers[0], activation=tf.nn.relu)
-        self.prenet2 = tf.keras.layers.Dense(units=prenet_layers[1], activation=tf.nn.relu)
-        self.dropout = tf.keras.layers.Dropout(dropout)
+        self.sampling_layer = tf.keras.layers.Dense(units=prenet_layers[0])
+        self.prenet_layers = []
+        for idx in range(len(prenet_layers)):
+            self.prenet_layers.append(tf.keras.layers.Dense(units=prenet_layers[idx], activation=tf.nn.relu))
+        self.prenet_dropout = tf.keras.layers.Dropout(dropout)
 
     def call(self, input_, training=True):
         """Run the model
             Args:
-                input_: 3-D tensor of shape [batch_size, timesteps, input_dim]
-            Returns:
-                output_: 3-D tensor of shape [batch_size, timesteps, prenet_layers[-1]]
+                 input_: 3-D tensor of shape [batch_size, timesteps, input_dim]
+             Returns:
+                 output_: 3-D tensor of shape [batch_size, timesteps, prenet_layers[-1]]
         """
-        output1 = self.dropout(self.prenet1(input_), training=training)
-        output2 = self.dropout(self.prenet2(output1), training=training)
+        # output of sampling layer
+        output_ = self.sampling_layer(input_)
+        
+        # output of prenet layers
+        for idx in range(len(self.prenet_layers)):
+            output_ = self.prenet_dropout(self.prenet_layers[idx](output_), training=training)
 
-        return output2
+        return output_
