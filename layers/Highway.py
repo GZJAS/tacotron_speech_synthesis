1b3adc49a8dc118691369dbde99cb02d45c0de73
Anandaswarup Vadapalli <anandaswarup@bombe.local>
2018-04-28 16:16:10 +0530

    Highway Network

--- layers/Highway.py
+++ layers/Highway.py
@@ -1,33 +1,44 @@
-"""Highway layer"""
+"""Highway Network"""
 
 import tensorflow as tf
 
 
-class HighwayLayer(tf.keras.Model):
-    """Highway Layer
+class HighwayNetwork(tf.keras.Model):
+    """Highway network
     """
-    def __init__(self,  num_units):
-        super(HighwayLayer, self).__init__()
+    def __init__(self, num_units, num_layers):
+        """Constructor
+        """
+        super(HighwayNetwork, self).__init__()
 
-        self.pre_highway = tf.keras.layers.Dense(units=num_units)
-        self.H = tf.keras.layers.Dense(units=num_units, activation=tf.nn.relu, use_bias=True)
-        self.T = tf.keras.layers.Dense(units=num_units, activation=tf.nn.sigmoid, use_bias=True,
-                                       bias_initializer=tf.constant_initializer(-1.0))
+        self.num_layers = num_layers
+        self.num_units = num_units
+
+        self.sampling_layer = tf.keras.layers.Dense(units=num_units)
+
+        self.Hs = []
+        self.Ts = []
+
+        for idx in range(self.num_layers):
+            self.Hs.append(tf.keras.layers.Dense(units=num_units, activation=tf.nn.relu))
+            self.Ts.append(tf.keras.layers.Dense(units=num_units, activation=tf.nn.sigmoid,
+                           bias_initializer=tf.constant_initializer(-1.0)))
 
     def call(self, input_):
-        """Run the model
+        """ Run the model
             Args:
-                input_: 3-D tensor of shape [batch_size, timesteps, input_dim]
-            Returns:
-                output_: 3-D tensor of shape [batch_size, timesteps, num_units]
+                 input_: 3-D tensor of shape [batch_size, timesteps, input_dim]
+             Returns:
+                 output_: 3-D tensor of shape [batch_size, timesteps, num_units]
         """
-        # up/down-sample if the dimensionality of input does not match highway layer size
-        if input_.get_shape()[-1] != self.num_units:
-            input_ = self.pre_highway(input_)
+        # output of sampling layer
+        output_ = self.sampling_layer(input_)
 
-        H_out = self.H(input_)
-        T_out = self.T(input_)
+        # output of highway layers
+        for idx in range(self.num_layers):
+            H_out = self.Hs[idx](output_)
+            T_out = self.Ts[idx](output_)
 
-        output_ = H_out * T_out + input_ * (1.0 - T_out)
+            output_ = H_out * T_out + output_ * (1.0 - T_out)
 
         return output_
